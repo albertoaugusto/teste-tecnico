917ed82ec7969dbfe92db66512316e80754cda15
Alberto <albertosouza16.contato@gmail.com>
2024-09-02 23:38:47 -0300

    feat: exercice 5 without reverse function

--- ex5.js
+++ ex5.js
@@ -1,7 +1,14 @@
-function invertString(s) {
-    return s.split('').reverse().join('');
+function stringInvert(str) {
+  let invertedText = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    invertedText += str[i];
+  }
+
+  return invertedText;
 }
 
-const text = prompt("Digite a string para inverter:");
-const invertedText = invertString(text);
-console.log(`String invertida: ${invertedText}`);
+const stringInverted = prompt("Digite a string para inverter:");
+const result = stringInvert(stringInverted);
+
+console.log(`String invertida: ${result}`);
